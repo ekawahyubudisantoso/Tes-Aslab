a2701471d129c17ceab6c63720988fb87f51996f
ekawahyubudisantoso <ekawahyubudisantoso22@gmail.com>
2019-02-07 19:48:15 +0700

    membuat web dinamis dan crud - first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Beranda</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="content">
+        <header>
+            <h1>WEBSITE APAAN INI ANJ*NG!!!?</h1>
+            <p>Ini Contoh website dinamis dengan CRUD</p>
+        </header>
+        <div class="menu">
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">TENTANG</a></li>
+                <li><a href="">TUTORIAL</a></li>
+            </ul>
+        </div>
+        <div class="badan">
+
+        </div>
+    </div>
+</body>
+</html>
